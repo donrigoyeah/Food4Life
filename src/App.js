dbe7d2a8fb872905b1e240a63c0567ab02ddfe9c
donrigoyeah <46565056+donrigoyeah@users.noreply.github.com>
2020-06-05 17:14:50 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,66 +2,53 @@ import React, {useState} from 'react';
 import './App.css';
 import Data from './json/recipes_2.json'
 
-//console.log('1: ' + Data)
-//console.log(Data.filter(dish => dish.type === 'haupt'))
-
-
-
 const App = props => {
   const [dataState, setdataState] = useState({
     Data
   });
 
+  // +++++ Work in Progress starts here: setdataState not working with filtered list.... ++++++ 
 
   const filterNameHandler = value => () => 
   
     console.log(Data.filter(dish => dish.type === value))
     
-    //console.log(Data)
-    //console.log(filterNameHandler.value)
-    //console.log(Data.filter(dish => dish.type === `${filterNameHandler.value}`))
-    
     setdataState({
       filteredData = Data.filter(dish => dish.type === value)
-    })  
-  ;
+    });
 
 
 
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1>Food4Life2</h1>
-        </header>
-
+  return (
+     <div className="App">
+       <header className="App-header">
+         <h1>Food4Life2</h1>
+       </header>
         <nav>
-          <button onClick={filterNameHandler('TippDesTages')}>Gericht des tages</button>
-          <button onClick={filterNameHandler('haupt')}>Hauptspeisen</button>
-          <button onClick={filterNameHandler('first')}>Vorspeisen</button>
-          <button onClick={filterNameHandler('dessert')}>Dessert</button>
-          <button onClick={filterNameHandler('haupt')}>Drink</button>
-        </nav>
-
+         <button onClick={filterNameHandler('TippDesTages')}>Gericht des tages</button>
+         <button onClick={filterNameHandler('haupt')}>Hauptspeisen</button>
+         <button onClick={filterNameHandler('first')}>Vorspeisen</button>
+         <button onClick={filterNameHandler('dessert')}>Dessert</button>
+         <button onClick={filterNameHandler('haupt')}>Drink</button>
+       </nav>
         <div className="MainBody">
-
           {/* ADD FILTER FUNCTION TO MAP FROM BUTTONS: {this.state.Data.filter().map(Data  => (...   */}
-
           {dataState.Data.map(Data  => (
-            <div className='Rezept' style={{backgroundImage: `url(${Data.picture})`, 
-                                            backgroundSize: 'cover', 
-                                            backgroundPosition: 'center',
-                                            width: '20%',
-                                            height: '15%',
-                                            margin: '30px',
-                                            padding: '50px',
-                                            display: 'flex'}}>
-              <h3 style={{color: 'white', backgroundColor: 'black', padding: '10px'}}>{Data.name} </h3>
-            </div>
-          ))}
-        </div>     
-      </div>
-    );
-  }
+           <div className='Rezept' style={{backgroundImage: `url(${Data.picture})`, 
+                                           backgroundSize: 'cover', 
+                                           backgroundPosition: 'center',
+                                           width: '20%',
+                                           height: '15%',
+                                           margin: '30px',
+                                           padding: '50px',
+                                           display: 'flex'}}>
+             <h3 style={{color: 'white', backgroundColor: 'black', padding: '10px'}}>{Data.name} </h3>
+           </div>
+         ))}
+       </div>     
+     </div>
+  );
+ }
 
 
 export default App;
