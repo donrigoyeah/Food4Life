642ea32a4660fdbef0f479134e5feffbe61dd3c0
donrigoyeah <46565056+donrigoyeah@users.noreply.github.com>
2020-06-15 16:37:52 +0200

    new commit
    yoo

--- src/App.js
+++ src/App.js
@@ -1,85 +1,160 @@
-import React, {useState} from 'react';
-import {Route, Link} from 'react-router-dom'
-import './App.css';
-import recipeData from './json/recipes_2.json'
+import React, { useState } from "react";
 
-const App = props => {
+import { Route, Link } from "react-router-dom";
+
+import "./App.css";
+import recipeData from "./json/recipes_2.json";
+
+import Modal from "./Modal";
+import useModal from "./useModal";
+
+console.log(recipeData);
+
+const App = (props) => {
   const [recipes, setRecipes] = useState(recipeData);
+  const { isShowing, toggle } = useModal();
 
-  const filterNameHandler = value => () => {
-    if (value == 'TippDesTages') {
-      const hauptRecipes = recipeData.filter(dish => dish.type === 'haupt')
-      const firstRecipes = recipeData.filter(dish => dish.type === 'first')
-      const dessertRecipes = recipeData.filter(dish => dish.type === 'dessert')
-      const drinkRecipes = recipeData.filter(dish => dish.type === 'drink') 
+  // ++++++
+  // Filter Function for Nav Bar
+  // ++++++
+  const filterNameHandler = (value) => () => {
+    if (value == "TippDesTages") {
+      const hauptRecipes = recipeData.filter((dish) => dish.type === "haupt");
+      const firstRecipes = recipeData.filter((dish) => dish.type === "first");
+      const dessertRecipes = recipeData.filter(
+        (dish) => dish.type === "dessert"
+      );
+      const drinkRecipes = recipeData.filter((dish) => dish.type === "drink");
 
       const TippDesTages = [];
-      TippDesTages.push(hauptRecipes[Math.floor(Math.random() * (hauptRecipes.length))])
-      TippDesTages.push(firstRecipes[Math.floor(Math.random() * (firstRecipes.length))])
-      TippDesTages.push(dessertRecipes[Math.floor(Math.random() * (dessertRecipes.length))])
-      TippDesTages.push(drinkRecipes[Math.floor(Math.random() * (drinkRecipes.length))])
-      setRecipes(
-        TippDesTages
-      )
+      TippDesTages.push(
+        hauptRecipes[Math.floor(Math.random() * hauptRecipes.length)]
+      );
+      TippDesTages.push(
+        firstRecipes[Math.floor(Math.random() * firstRecipes.length)]
+      );
+      TippDesTages.push(
+        dessertRecipes[Math.floor(Math.random() * dessertRecipes.length)]
+      );
+      TippDesTages.push(
+        drinkRecipes[Math.floor(Math.random() * drinkRecipes.length)]
+      );
+      setRecipes(TippDesTages);
+    } else if (Number.isInteger(value)) {
+      const filteredRecipes = recipeData.filter(
+        (recipe) => recipe.id === value
+      );
+      setRecipes(filteredRecipes);
+    } else if (value === "alle") {
+      const filteredRecipes = recipeData;
+      setRecipes(filteredRecipes);
+    } else {
+      const filteredRecipes = recipeData.filter(
+        (recipe) => recipe.type === value
+      );
+      setRecipes(filteredRecipes);
     }
+  };
+  //console.log("currentRecipes", recipes);
 
-    else if (Number.isInteger(value)){
-      const filteredRecipes = recipeData.filter(recipe => recipe.id === value)
-      setRecipes(
-        filteredRecipes
-      )
+  // ++++++
+  // Form Function for Edit Recipe Page
+  // ++++++
+  const formHandler = (event) => {
+    this.setState({ username: event.target.value });
+  };
 
-    } else if (value === 'alle'){
-      const filteredRecipes = recipeData
-      setRecipes(
-        filteredRecipes
-      )
-    } else  {
-      const filteredRecipes = recipeData.filter(recipe => recipe.type === value)
-      setRecipes(
-        filteredRecipes
-    );
-   }
-  }
-
-console.log("currentRecipes", recipes);
-
-
-  if (recipes.length === 1){
-    // +++++
-    // +++++     Single Page Recipe Display
-    // +++++
+  // +++++
+  // +++++     Single Page Recipe Display
+  // +++++
+  if (recipes.length === 1) {
     return (
       <div className="App">
         <header className="App-header">
           <h1>Food4Life2</h1>
         </header>
-
-          <nav>
-          <button onClick={filterNameHandler('TippDesTages')}>Gericht des tages</button>
-          <button onClick={filterNameHandler('haupt')}>Hauptspeisen</button>
-          <button onClick={filterNameHandler('first')}>Vorspeisen</button>
-          <button onClick={filterNameHandler('dessert')}>Dessert</button>
-          <button onClick={filterNameHandler('drink')}>Drink</button>
-          <button onClick={filterNameHandler('alle')}>Alle Rezepte</button>
+        <nav>
+          <button onClick={filterNameHandler("TippDesTages")}>
+            Gericht des tages
+          </button>
+          <button onClick={filterNameHandler("haupt")}>Hauptspeisen</button>
+          <button onClick={filterNameHandler("first")}>Vorspeisen</button>
+          <button onClick={filterNameHandler("dessert")}>Dessert</button>
+          <button onClick={filterNameHandler("drink")}>Drink</button>
+          <button onClick={filterNameHandler("alle")}>Alle Rezepte</button>
         </nav>
+        <div className="MainBody" style={{ justifyContent: "center" }}>
+          {recipes.map((recipe, index) => (
+            <div className="SingleRezept">
+              <h3
+                style={{
+                  color: "white",
+                  backgroundColor: "black",
+                  padding: "10px",
+                }}
+              >
+                {recipe.name}{" "}
+              </h3>
+              <div className="SingleRezeptItem">
+                <p>Zutaten:</p>
+                {/* 
+                ++++ should loop through the indicrients and renders them as paragraphes
+                {recipe.ingredients.map(recipe) => (<p>{recipe.ingredients}</p>)} 
+                ++++
 
-          <div className="MainBody">
-            {recipes.map((recipe, index)  => (
-                  <div className='SingleRezept'>
-                  <h3 style={{color: 'white', backgroundColor: 'black', padding: '10px'}}>{recipe.name} </h3>
-                  <ol>
-                    <li>Zutaten:{recipe.ingredients}</li>
-                    <br></br>
-                    <li>Zubereitung:{recipe.description}</li>
-                    <br></br>
-                    <li>Bewertung:{recipe.rating}/5</li>
-                  </ol>          
-                </div>                   
+                ++++ also does not work
+                {console.log(
+                  recipe.ingredients.map((ingredients, index) => {
+                    <p key={index}> {ingredients}</p>;
+                  })
+                )}
+                ++++
+                */}
+                <p>{recipe.ingredients}</p>
+              </div>
+              <div className="SingleRezeptItem">
+                <p>Zubereitung</p>
+                {/* 
+                ++++ should loop through the decribtion and renders them as paragraphes
+                {recipe.description.map(recipe) => (<p>{recipe.describtion}</p>)} 
+                ++++
+
+                ++++ also does not work
+                {console.log(
+                  recipe.describtion.map((describtion, index) => {
+                    <p key={index}> {describtion}</p>;
+                  })
+                )}
+                ++++
+                */}
+                <p>{recipe.description}</p>
+              </div>
+              <div className="SingleRezeptItem">
+                <p>Bewertung:</p>
+                <p>{recipe.rating}/5</p>
+              </div>
+              <div className="SingleRezeptItem">
+                <p>Severity:</p>
+                <p>{recipe.severity}/5</p>
+              </div>
+              <div className="SingleRezeptItem">
+                <p>Time:</p>
+                <p>{recipe.time}</p>
+              </div>
+              <button className="button-default" onClick={toggle}>
+                Edit
+              </button>
+              {console.log(recipe)}
+              <Modal isShowing={isShowing} hide={toggle} recipe={recipe} />
+              <button>Delete</button>
+              <div className="SingleRecipeImage">
+                <img src={recipe.picture}></img>
+              </div>
+            </div>
           ))}
-        </div>     
+        </div>
       </div>
-    )
+    );
   } else {
     // +++++
     // +++++     Overview of Recipes
@@ -90,43 +165,62 @@ console.log("currentRecipes", recipes);
           <h1>Food4Life2</h1>
         </header>
 
-          <nav>
-          <button onClick={filterNameHandler('TippDesTages')}>Gericht des tages</button>
-          <button onClick={filterNameHandler('haupt')}>Hauptspeisen</button>
-          <button onClick={filterNameHandler('first')}>Vorspeisen</button>
-          <button onClick={filterNameHandler('dessert')}>Dessert</button>
-          <button onClick={filterNameHandler('drink')}>Drink</button>
-          <button onClick={filterNameHandler('alle')}>Alle Rezepte</button>
+        <nav>
+          <button onClick={filterNameHandler("TippDesTages")}>
+            Gericht des tages
+          </button>
+          <button onClick={filterNameHandler("haupt")}>Hauptspeisen</button>
+          <button onClick={filterNameHandler("first")}>Vorspeisen</button>
+          <button onClick={filterNameHandler("dessert")}>Dessert</button>
+          <button onClick={filterNameHandler("drink")}>Drink</button>
+          <button onClick={filterNameHandler("alle")}>Alle Rezepte</button>
         </nav>
 
-          <div className="MainBody">
-            {recipes.map((recipe, index)  => (
-
-              //   ++++  Eric added this "link to" element which is supposed to work as a rout to recipe
-              //         <Link to={`/recipes/${recipe.id}`}>
-                  <div className='Rezept' onClick={filterNameHandler(recipe.id)}
-                                        style={{backgroundImage: `url(${recipe.picture})`, 
-                                                backgroundSize: 'cover', 
-                                                backgroundPosition: 'center',
-                                                width: '20%',
-                                                height: '15%',
-                                                margin: '30px',
-                                                padding: '50px',
-                                                display: 'flex',
-                                                }}>
-                  <h3 style={{color: 'white', backgroundColor: 'black', padding: '10px'}}>{recipe.name} </h3>
-                                
-                </div>
-              //</Link>                        
+        <div className="MainBody" style={{ justifyContent: "center" }}>
+          {recipes.map((recipe, index) => (
+            //   ++++  Eric added this "link to" element which is supposed to work as a rout to recipe
+            //         <Link to={`/recipes/${recipe.id}`}>
+            <div
+              className="Rezept"
+              onClick={filterNameHandler(recipe.id)}
+              style={{
+                backgroundImage: `url(${recipe.picture})`,
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+                width: "20%",
+                height: "15%",
+                margin: "30px",
+                padding: "50px",
+                display: "flex",
+              }}
+            >
+              <h3
+                style={{
+                  color: "white",
+                  backgroundColor: "black",
+                  padding: "10px",
+                }}
+              >
+                {recipe.name}{" "}
+              </h3>
+            </div>
+            //</Link>
           ))}
-        </div>     
+        </div>
+
+        <div className="Loader">
+          <button
+            onClick={localStorage.setItem(
+              "recipes",
+              JSON.stringify(recipeData)
+            )}
+          >
+            Load JSON to local Storage
+          </button>
+        </div>
       </div>
     );
   }
- 
- 
-  }// closes <div> App
-
-
+}; // closes <div> App
 
 export default App;
